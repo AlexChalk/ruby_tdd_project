a4d26e7e924c4f249f48a692871abb709515b990
Alex Chalk <alexchalk17@gmail.com>
2017-02-02 13:36:28 -0500

    Make first two tests pass.

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # ruby_tdd_project
+
+This is a project to improve and showcase my abilities with test driven development in ruby.
---
+++ learn_ruby/01_temperature/temperature.rb
@@ -0,0 +1,8 @@
+#write your code here
+def ftoc number
+  ((number-32.0) / (9.0/5.0)).round(2) 
+end
+
+def ctof number
+  (number * (9.0/5.0) + 32).round(2) 
+end
