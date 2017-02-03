8375beabeea1f61f6e54661cfe9cb91abff13e79
Alex Chalk <alexchalk17@gmail.com>
2017-02-02 19:46:57 -0500

    Make 8 more tests pass.

--- learn_ruby/02_calculator/calculator.rb
+++ learn_ruby/02_calculator/calculator.rb
@@ -1,1 +1,20 @@
 #write your code here
+def add x, y
+  x + y
+end
+
+def subtract x, y
+  x - y
+end
+
+def sum array
+  total = 0
+  array.each do |x|
+    total += x
+  end
+  total
+end
+
+def multiply a, b
+  a * b
+end
--- learn_ruby/02_calculator/calculator_spec.rb
+++ learn_ruby/02_calculator/calculator_spec.rb
@@ -77,11 +77,16 @@ end
 # once the above tests pass,
 # write tests and code for the following:
 
-describe "#multiply" do
+describe "multiply" do
 
-  it "multiplies two numbers"
+  it "multiplies two numbers" do
+    expect(multiply(15, 17)).to eq(255)
+  end
 
-  it "multiplies several numbers"
+  it "multiplies several numbers" do
+    #expect(multiply(15, 16, -234, 586.22)).to eq(-32_922_115.2)
+    expect(multiply(15, 16, -234)).to eq(-56_160)
+  end
   
 end
 
