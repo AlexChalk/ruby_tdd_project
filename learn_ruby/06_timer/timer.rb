a83f4d4eae2479a3f6ee0f86193624f521558baa
Alex Chalk <alexchalk17@gmail.com>
2017-02-06 10:50:44 -0500

    Add timer solution.

--- learn_ruby/06_timer/timer.rb
+++ learn_ruby/06_timer/timer.rb
@@ -1,3 +1,25 @@
 class Timer
-  #write your code here
+
+  attr_accessor :seconds
+
+  def initialize
+    @seconds = 0
+  end
+
+  def time_string
+    hours = @seconds/3600
+    minutes = @seconds/60 - hours*60
+    seconds = @seconds - hours*3600 - minutes*60
+    time = [hours, minutes, seconds]
+    time.collect! do |x| 
+      if x < 9
+        x = "0" + x.to_s
+      else
+        x = x.to_s
+      end
+    end
+    
+    @time_string = time.join(":")
+  end
+
 end
